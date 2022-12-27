de3e111106d3a02943e7c2a7063e9785164f27b3
sotanmochi <16936357+sotanmochi@users.noreply.github.com>
2022-12-28 03:54:43 +0900

    Add assets for representative color

---
+++ Packages/jp.sotanmochi.prel/Samples~/RepresentativeColor/RealtimeImageProcessing/Scripts/RealtimeImageProcessing.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace Prel.RepresentativeColor.Samples
+{
+    public class RealtimeImageProcessing : MonoBehaviour
+    {
+        [SerializeField] HighIntensityRepresentativeColorEstimator _ColorEstimator;
+        [SerializeField] Renderer InputImageObject;
+        [SerializeField] Renderer GrayscaleImageObject;
+        [SerializeField] Renderer VisualizeImageObject;
+        [SerializeField] Renderer CutLowOutputImageObject;
+        [SerializeField] Renderer AverageColorImageObject;
+
+        void Start()
+        {
+            Texture srcTexture = InputImageObject.material.mainTexture;
+            _ColorEstimator.Initialize(srcTexture);
+
+            GrayscaleImageObject.material.mainTexture = _ColorEstimator.GrayscaleTexture;
+            VisualizeImageObject.material.mainTexture = _ColorEstimator.VisualizationTex;
+            CutLowOutputImageObject.material.mainTexture = _ColorEstimator.CutLowDstTexture;
+
+            _ColorEstimator.OnUpdate += OnUpdate;
+        }
+
+        void OnDestroy()
+        {
+            _ColorEstimator.OnUpdate -= OnUpdate;
+        }
+
+        void OnUpdate(Color color)
+        {
+            AverageColorImageObject.material.color = color;
+        }
+    }
+}
