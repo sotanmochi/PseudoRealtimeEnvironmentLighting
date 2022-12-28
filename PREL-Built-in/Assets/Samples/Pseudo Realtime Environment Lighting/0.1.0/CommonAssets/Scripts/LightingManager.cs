81ab97971a64e1c5a4f90de9359004959158e7dd
sotanmochi <16936357+sotanmochi@users.noreply.github.com>
2022-12-28 11:47:28 +0900

    Update project

---
+++ PREL-Built-in/Assets/Samples/Pseudo Realtime Environment Lighting/0.1.0/CommonAssets/Scripts/LightingManager.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+using UniRx;
+using Prel.RepresentativeColor;
+using Prel.MaterialCapture;
+
+namespace Prel.Samples
+{
+    public class LightingManager : MonoBehaviour
+    {
+        [SerializeField] Light _DirectionalLight;
+        [SerializeField] RealtimeEnvironmentLight _RealtimeEnvironmentLight;
+        [SerializeField] HighIntensityRepresentativeColorEstimator _LightColorEstimator;
+        [SerializeField] Texture _LightColorEstimationSource;
+        [SerializeField] Material _EmissiveFloorMaterial;
+        [SerializeField] Material _EmissiveQuadMaterial;
+
+        public bool EnableLightColorEstimator = true;
+
+        private Color _EmissionBaseColor = new Color(1f, 1f, 1f);
+
+        public void Initialze()
+        {
+            _LightColorEstimator.Initialize(_LightColorEstimationSource);
+
+            _LightColorEstimator.OnUpdate += (color) =>
+            {
+                if (EnableLightColorEstimator)
+                {
+                    _DirectionalLight.color = color;
+                }
+                else
+                {
+                    _DirectionalLight.color = Color.white;
+                }
+            };
+        }
+
+        public void SetEnableLightColorEstimator(bool value)
+        {
+            EnableLightColorEstimator = value;
+        }
+
+        public void SetEnableDirectionalLight(bool value)
+        {
+            _DirectionalLight.gameObject.SetActive(value);
+            _DirectionalLight.enabled = value;
+        }
+
+        public void SetDirectionalLightIntensity(float intensity)
+        {
+            _DirectionalLight.intensity = intensity;
+        }
+
+        public void SetEnableEnvironmentLight(bool value)
+        {
+            if (_RealtimeEnvironmentLight != null)
+            {
+                _RealtimeEnvironmentLight.EnableEnvironmetLighting = value;
+            }
+        }
+
+        public void SetEnvironmentLightMaxIntensity(float maxIntensity)
+        {
+            if (_RealtimeEnvironmentLight != null)
+            {
+                _RealtimeEnvironmentLight.MaxIntensity = maxIntensity;
+            }
+        }
+
+        public void SetEnvironmentLightMetallic(float value)
+        {
+            if (_RealtimeEnvironmentLight != null)
+            {
+                _RealtimeEnvironmentLight.Metallic = value;
+            }
+        }
+
+        public void SetEnvironmentLightSmoothness(float value)
+        {
+            if (_RealtimeEnvironmentLight != null)
+            {
+                _RealtimeEnvironmentLight.Smoothness = value;
+            }
+        }
+
+        public void SetEmissiveFloorIntensity(float value)
+        {
+            Color color = value * _EmissionBaseColor;
+            _EmissiveFloorMaterial.SetColor("_EmissionColor", color);
+        }
+
+        public void SetEmissiveQuadIntensity(float value)
+        {
+            Color color = value * _EmissionBaseColor;
+            _EmissiveQuadMaterial.SetColor("_EmissionColor", color);
+        }
+    }
+}
---
+++ PREL-Built-in/Assets/Samples/Pseudo Realtime Environment Lighting/0.1.0/CommonAssets/Scripts/LightingPresenter.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using UniRx;
+
+namespace Prel.Samples
+{
+    public class LightingPresenter : MonoBehaviour
+    {
+        [SerializeField] LightingView _LightingView;
+        [SerializeField] LightingManager _LightingManager;
+
+        void Awake()
+        {
+            _LightingManager.Initialze();
+
+            _LightingView.EnableEnvironmentLight
+            .Subscribe(value => 
+            {
+                _LightingManager.SetEnableEnvironmentLight(value);
+            })
+            .AddTo(this);
+
+            _LightingView.EnableDirectionalLight
+            .Subscribe(value => 
+            {
+                _LightingManager.SetEnableDirectionalLight(value);
+            })
+            .AddTo(this);
+
+            _LightingView.EnableLightColorEstimator
+            .Subscribe(value => 
+            {
+                _LightingManager.SetEnableLightColorEstimator(value);
+            })
+            .AddTo(this);
+
+            _LightingView.EnvironmentLightMaxIntensity
+            .Subscribe(value => 
+            {
+                _LightingManager.SetEnvironmentLightMaxIntensity(value);
+            })
+            .AddTo(this);
+
+            _LightingView.Metallic
+            .Subscribe(value => 
+            {
+                _LightingManager.SetEnvironmentLightMetallic(value);
+            })
+            .AddTo(this);
+
+            _LightingView.Smoothness
+            .Subscribe(value => 
+            {
+                _LightingManager.SetEnvironmentLightSmoothness(value);
+            })
+            .AddTo(this);
+
+            _LightingView.DirectionalLightIntensity
+            .Subscribe(value => 
+            {
+                _LightingManager.SetDirectionalLightIntensity(value);
+            })
+            .AddTo(this);
+
+            _LightingView.EmissiveFloorIntensity
+            .Subscribe(value => 
+            {
+                _LightingManager.SetEmissiveFloorIntensity(value);
+            })
+            .AddTo(this);
+
+            _LightingView.EmissiveQuadIntensity
+            .Subscribe(value => 
+            {
+                _LightingManager.SetEmissiveQuadIntensity(value);
+            })
+            .AddTo(this);
+        }
+    }
+}
